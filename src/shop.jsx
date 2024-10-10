460c81465e7111792e5bd7a7bc3eac859d61675a
s1r1luxx <155615882+s1r1luxx@users.noreply.github.com>
2024-10-10 08:21:21 +0000

    Connect with Backend

--- src/shop.jsx
+++ src/shop.jsx
@@ -1,6 +1,19 @@
 import './shop.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 export default function Shop() {
+    const [products, setProducts] = useState([]);
+    const URL="https://effective-guide-pjjq5xv46xpqc5xj-5000.app.github.dev";
+    useEffect(()=>{
+        axios.get(URL+'/api/products')
+        .then(response=>{
+            setProducts(response.data);
+        })
+        .catch(error=>{
+            console.log("error");
+        });
+    }
+    ,[]);
     const Item=(props)=>{
         return (
             <div key={props.id} onClick={()=>props.callback(props)}>
@@ -11,14 +24,6 @@ export default function Shop() {
             </div>
         );
     }
-    const products = [
-        {id: 0, name: "Notebook Acer Swift",price:45900,img:"https://img.advice.co.th/images_nas/pic_product4/A0147295/A0147295_s.jpg"},
-        {id: 1, name: "Notebook Asus Vivo",price:19900,img:"https://img.advice.co.th/images_nas/pic_product4/A0146010/A0146010_s.jpg"},
-        {id: 2, name: "Notebook Lenovo Ideapad",price:32900,img:"https://img.advice.co.th/images_nas/pic_product4/A0149009/A0149009_s.jpg"},
-        {id: 3, name: "Notebook MSI Prestige",price:54900,img:"https://img.advice.co.th/images_nas/pic_product4/A0149954/A0149954_s.jpg"},
-        {id: 4, name: "Notebook DELL XPS",price:99900,img:"https://img.advice.co.th/images_nas/pic_product4/A0146335/A0146335_s.jpg"},
-        {id: 5, name: "Notebook HP Envy",price:46900,img:"https://img.advice.co.th/images_nas/pic_product4/A0145712/A0145712_s.jpg"}
-    ];
     
     const [cart, setCart] = useState([]);
     function addCart(item){
